fb3bae20315c2751b3e9c3166a889ee00951aad1
CG_LEON <guillen.anton@gmail.com>
2018-04-24 14:00:39 -0500

    Finalizado
    Las cuatro funciones ya estan listas y funcionando.

--- proyectoFinal/js/funciones.js
+++ proyectoFinal/js/funciones.js
@@ -1,14 +1,14 @@
 var estudiante = [
-{"codigo" : "01", "nombre" : "Carlos", "nota" : 10},
-{"codigo" : "02", "nombre" : "Luz", "nota" : 9 },
+{"codigo" : "01", "nombre" : "Carlos", "nota" : 9},
+{"codigo" : "02", "nombre" : "Luz", "nota" : 9},
 {"codigo" : "03", "nombre" : "Karen", "nota" : 8},
 {"codigo" : "04", "nombre" : "Yuri", "nota" : 7},
-{"codigo" : "05", "nombre" : "Joe", "nota" : 6},
+{"codigo" : "05", "nombre" : "Joe", "nota" : 8},
 {"codigo" : "06", "nombre" : "Chavita", "nota" : 5},
 {"codigo" : "07", "nombre" : "Sandy", "nota" : 6},
-{"codigo" : "08", "nombre" : "Piti", "nota" : 10},
+{"codigo" : "08", "nombre" : "Piti", "nota" : 8},
 {"codigo" : "09", "nombre" : "Jatzy", "nota" : 9},
-{"codigo" : "10", "nombre" : "Angie", "nota" : 8}
+{"codigo" : "10", "nombre" : "Angie", "nota" : 1}
 ];
 
 function mostrarDatos(json){
@@ -29,7 +29,47 @@ var output = "<h3>Datos de los alumnos</h3> <br>";
 output += "</table>";
 document.getElementById("panel").innerHTML = output;
 }
+function promedio(json){
+	var promedio = 0;
+	var suma =0;
+	for (var i = 0; i < json.length; i++) {
+		suma += json[i].nota;
+	}
+	promedio = suma/10;
+	document.getElementById("panel").innerHTML = promedio;
+}
+function mayorNota(json){
+	var out = "<h3> Mayor Nota </h3>";
+	var pos = 0;
+
+	for (var i = 0; i < json.length; i++) {
+		if (json[i].nota > pos) {
+			pos = json[i].nota;
+		}
+	}
+	out += "La nota mayor es: " + pos;
+	document.getElementById("panel").innerHTML = out;
+}
+function menorNota(json){
+	var out = "<h3>Menor Nota </h3>"
+	var pos = json[0].nota;
+	for (var i = 0; i < json.length; i++) {
+		if (json[i].nota < pos) {
+			pos = json[i].nota
+		}
+	}
+	out += "La nota menor es de: " + pos;
+	document.getElementById("panel").innerHTML = out;
+}
 function mostrarEstudiantes(){
 	mostrarDatos(estudiante);
 }
-//"Codigo: " + json[i].codigo + " Nombre: " + json[i].nombre + " Nota: " + json[i].nota + "<br>";
+function mostrarPromedio(){
+	promedio(estudiante);
+}
+function mostrarMayor(){
+	mayorNota(estudiante);
+}
+function mostrarMenor(){
+	menorNota(estudiante);
+}
